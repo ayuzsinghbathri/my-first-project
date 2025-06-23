95735e70c506238c174a781160d88f38fd708687
ayuzsinghbathri <142677501+ayuzsinghbathri@users.noreply.github.com>
2025-06-23 11:38:47 +0000

    02_datatype.js

---
+++ .devcontainer/02_dataType.js
@@ -0,0 +1,13 @@
+"use stricrt";//treat all js code as newer version
+// alert(3+3) we are using nodejs not browser
+console.log(typeof null);
+console.log(typeof undefined);
+/*
+number = 2 to power 52
+bigint
+string = ""
+boolean = true/false
+null = standalone value
+undefined
+symbol = unique
+*/
